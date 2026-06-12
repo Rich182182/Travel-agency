c6043a9171c2d072000b6339069a5b6c6a51033a
Liza <rabirokhliza@gmail.com>
2026-06-12 21:15:29 +0300

    Add RoomType enum mappings

--- TravelAgency/TravelAgency.BLL/Mapping/RoomDomainProfile.cs
+++ TravelAgency/TravelAgency.BLL/Mapping/RoomDomainProfile.cs
@@ -11,6 +11,12 @@ namespace TravelAgency.BLL.Mapping
             CreateMap<Room, RoomDto>().ReverseMap();
             CreateMap<CreateRoomDto, Room>().ReverseMap();
             CreateMap<UpdateRoomDto, Room>().ReverseMap();
+
+            CreateMap<DAL.Entities.Enums.RoomType, BLL.DTOs.Enums.RoomType>()
+                .ConvertUsing(x => (BLL.DTOs.Enums.RoomType)x);
+
+            CreateMap<BLL.DTOs.Enums.RoomType, DAL.Entities.Enums.RoomType>()
+                .ConvertUsing(x => (DAL.Entities.Enums.RoomType)x);
         }
     }
 }
